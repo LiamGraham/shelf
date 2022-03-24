07067bf6f2f3fa7e9d4e317223bf322b832f3703
Liam Graham <liam.david.graham@gmail.com>
2022-03-24 16:04:07 +1000

    Add `shelf()` to provide a default shell loop

--- shelf.py
+++ shelf.py
@@ -1,10 +1,15 @@
+import difflib
 import inspect
-from typing import Callable
+from nis import match
+from shutil import ExecError
+from typing import Callable, List
 from collections import OrderedDict
 
-class CommandError(Exception):
+class CommandParsingError(Exception):
     pass
 
+class NoMatchingCommandError(Exception):
+    pass
 
 class Command:
 
@@ -44,11 +49,11 @@ class Command:
                 command arguments
         """
         self.prefix = prefix
-        self._prefix_len = len(prefix.split(" "))
+        self._prefix_len = len(prefix.split())
         self.func = func
         self._args = OrderedDict()
         
-        self._signature = prefix
+        self.signature = prefix
         parameters = inspect.signature(func).parameters
         for name, param in parameters.items():
             if name == "self":
@@ -58,7 +63,7 @@ class Command:
             else:
                 annotation = param.annotation
             self._args[name] = annotation
-            self._signature += f" <{name}:{annotation.__name__}>"
+            self.signature += f" <{name}:{annotation.__name__}>"
 
 
     def parse(self, text:str):
@@ -72,10 +77,10 @@ class Command:
             Result of function call using command arguments
         """
         text = text.strip()
-        elements = text.split(" ")[self._prefix_len:]
+        elements = text.split()[self._prefix_len:]
          
         if not self.matches(text, elements):
-            raise CommandError(f"\"{text}\" does not match \"{self._signature}\"")
+            raise CommandParsingError(f"\"{text}\" does not match \"{self.signature}\"")
         
         arg_values = {}
         arg_order = []
@@ -87,7 +92,7 @@ class Command:
                 arg_values[arg] = value 
                 arg_order.append(value) 
             except (ValueError, TypeError):
-                raise CommandError(f"\"{target}\" invalid as \"{arg}\" for \"{self._signature}\"")
+                raise CommandParsingError(f"\"{target}\" invalid as \"{arg}\" for \"{self.signature}\"")
         return self.func(**arg_values)
 
     def partial(self, text:str):
@@ -109,7 +114,7 @@ class Command:
         return self.partial(text) and len(self._args) == len(elements)
 
     def __repr__(self) -> str:
-        return f"Command(signature=\"{self._signature}\")"
+        return f"Command(signature=\"{self.signature}\")"
 
     def __str__(self) -> str:
         return self.__repr__()
@@ -121,7 +126,8 @@ class CommandParser:
 
     def __init__(self):
         """Creates a new, empty CommandParser"""
-        self.commands = []
+        self.commands: List[Command] = []
+        self.prefixes = {}
 
     def add_command(self, prefix:str, func:Callable):
         """Adds a command to this parser having the specified prefix and based
@@ -134,6 +140,7 @@ class CommandParser:
         """
         command = Command(prefix, func)
         self.commands.append(command)
+        self.prefixes[command.prefix] = command
 
     def parse(self, text:str):
         """Parses the given command text. See Command.parse for more details.
@@ -148,5 +155,63 @@ class CommandParser:
             if command.partial(text):
                 return command.parse(text)
         else:
-            raise CommandError(f"\"{text}\" does not match any command")
+            raise NoMatchingCommandError(f"\"{text}\" does not match any command")
+    
+    def get_close_commands(self, text: str, n: int = 3, cutoff: float = 0.3) -> Command:
+        """Returns the `n` commands from the the list of registered commands which are
+        sufficiently similar to the given text.
 
+        Arguments:
+            text {str}: Text to compared against command prefixes
+            n {int}: Maximum number of commands to return
+            cutoff {float}: Similarity cutoff. Possibilities that don't score at least that 
+                similar to word are ignored
+
+        Returns:
+            Commands which are sufficiently similar to the given text 
+        """
+        if not text:
+            return []
+
+        # By only using the initial 'word' within the command, and possibly omitting subsequent words
+        # that might match the prefix of a valid command, we might capture other similar commands to then
+        # suggest
+        simple_prefix = text.split()[0]
+        matches = difflib.get_close_matches(simple_prefix, self.prefixes.keys(), n, cutoff)
+        return [self.prefixes[prefix] for prefix in matches] 
+
+def shelf(parser: CommandParser, start_symbol: str = "> ", exit_command: str = "exit"):
+    """Initiates a shell using the given CommandParser. `start_symbol` is displayed before
+    each new command ("> " by default). The shell can be exited by entering the given 
+    `exit_command` ("exit" by default) or the ^C (KeyboardInterrupt) or ^D (EOF) 
+    signals. 
+    
+    Arguments:
+        parser {CommandParser} Parser with which user-entered commands will be parsed
+        start_symbol {str}: String displayed at the start of each command
+        exit_command {str}: Command that may be entered to exit the shell
+    """
+    while True:
+        try:
+            command = input(start_symbol).strip()
+        except (KeyboardInterrupt, EOFError) as e:
+            print()
+            break
+
+        if command == "":
+            continue
+        if command == exit_command:
+            break
+        try:
+            output = parser.parse(command)
+            print(output)
+        except NoMatchingCommandError as e:
+            print("Unknown command:", e)
+            closest_commands = parser.get_close_commands(command)
+            if closest_commands:
+                suggestions = "\n".join([f"  - {command.signature}" for command in closest_commands])
+                print(f"\nThe most similar commands are:\n{suggestions}\n")
+        except CommandParsingError as e:
+            print(f"Invalid command: {e}\n")
+        except Exception as e:
+            print(f"Something went wrong. Error: {e}\n")
